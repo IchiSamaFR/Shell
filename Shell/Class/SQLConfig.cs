0f67df020f47b3a25a01be3dd6b889089199fd2a
IchiSamaFR <djordan-dsousa@hotmail.fr>
2021-06-09 11:54:13 +0200

    .gitignore changes

---
+++ Shell/Class/SQLConfig.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shell.Class
+{
+    public class SQLConfig
+    {
+        public string a_server = "";
+        public string a_db = "";
+        public string a_user = "";
+        public string a_pwd = "";
+
+        public string ConnString
+        {
+            get => "server=" + a_server + ";database=" + a_db + ";uid=" + a_user + ";pwd=" + a_pwd;
+        }
+
+        public void Init(string server, string db, string user, string pwd)
+        {
+            a_server = server;
+            a_db = db;
+            a_user = user;
+            a_pwd = pwd;
+        }
+    }
+}
